c0ddd628bcb5e72a7851d38b391c81bf071580f6
s-117-29 <s-117-29@gumiforest.ae.krakow.pl>
2022-04-20 18:12:38 +0200

    initial commit

---
+++ viteproject/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+import data from '../data.js'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+var products = {
+  apple: 2.5,
+  pear: 4,
+  grape: 0.1
+};
+</script>
+
+<template>
+  <p> ilosc towarow w koszyku: {{count}}</p>
+  <p> lista towarow: </p>
+<hr>
+
+   <p v-for="d of data">Nazwa: {{ d.name }}, Cena: {{ d.price }}, <button onclick="myFunction(){d.count++, count++}">Click me</button>, {{d.count=0}} </p>
+
+<hr>
+  
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ viteproject/src/data.js
@@ -0,0 +1,7 @@
+import { ref } from 'vue'
+export default ref([
+    {  name: 'Apple', price: 20},
+    {  name: 'Pear', price: 15},
+    { name: 'Grape', price: 64},
+    { name: 'Tomato', price: 32},
+])
